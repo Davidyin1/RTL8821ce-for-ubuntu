2db2c6ded7cc03c8d25896100909bf9401ad4ec3
LaurentYin <954424473@qq.com>
2020-10-23 11:57:40 +0800

    RTL

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# RTL8821ce-for-ubuntu
+网卡Realtek RTL8821CE linux系统的驱动程序
+
+我的内核版本好是 5.0.0-32-generic，（亲测5.0.0-23-generic ~ 5.0.0-32-generic都没问题)
+
+
+
+需要关闭Secure Boot，根据不同的电脑，关闭Secure Boot的操作不同。
+
+
+
+```
+修改Makefile里面的
+TopDIR ?= "文件夹当前绝对路径，根据你的情况而定, 这里采用命令读取当前路径，应该没问题(反正我这里没有)"
+
+sudo make
+
+sudo make install
+
+sudo modprobe -a 8821ce
+
+sudo reboot
+重启就可以看到wifi了
+```
+
+
+
---
+++ core/mesh/.rtw_mesh_pathtbl.o.cmd
@@ -0,0 +1,24 @@
+cmd_/home/laurentyin/RTL8821ce/Driver-Realtek-RTL8821CE/core/mesh/rtw_mesh_pathtbl.o := gcc -Wp,-MD,/home/laurentyin/RTL8821ce/Driver-Realtek-RTL8821CE/core/mesh/.rtw_mesh_pathtbl.o.d  -nostdinc -isystem /usr/lib/gcc/x86_64-linux-gnu/7/include  -I./arch/x86/include -I./arch/x86/include/generated  -I./include -I./arch/x86/include/uapi -I./arch/x86/include/generated/uapi -I./include/uapi -I./include/generated/uapi -include ./include/linux/kconfig.h -Iubuntu/include  -include ./include/linux/compiler_types.h -D__KERNEL__ -Wall -Wundef -Werror=strict-prototypes -Wno-trigraphs -fno-strict-aliasing -fno-common -fshort-wchar -fno-PIE -Werror=implicit-function-declaration -Werror=implicit-int -Wno-format-security -std=gnu89 -mno-sse -mno-mmx -mno-sse2 -mno-3dnow -mno-avx -m64 -falign-jumps=1 -falign-loops=1 -mno-80387 -mno-fp-ret-in-387 -mpreferred-stack-boundary=3 -mskip-rax-setup -mtune=generic -mno-red-zone -mcmodel=kernel -DCONFIG_X86_X32_ABI -DCONFIG_AS_CFI=1 -DCONFIG_AS_CFI_SIGNAL_FRAME=1 -DCONFIG_AS_CFI_SECTIONS=1 -DCONFIG_AS_SSSE3=1 -DCONFIG_AS_AVX=1 -DCONFIG_AS_AVX2=1 -DCONFIG_AS_AVX512=1 -DCONFIG_AS_SHA1_NI=1 -DCONFIG_AS_SHA256_NI=1 -Wno-sign-compare -fno-asynchronous-unwind-tables -mindirect-branch=thunk-extern -mindirect-branch-register -fno-jump-tables -fno-delete-null-pointer-checks -Wno-frame-address -Wno-format-truncation -Wno-format-overflow -O2 --param=allow-store-data-races=0 -Wframe-larger-than=1024 -fstack-protector-strong -Wno-unused-but-set-variable -Wimplicit-fallthrough -Wno-unused-const-variable -fno-omit-frame-pointer -fno-optimize-sibling-calls -fno-var-tracking-assignments -pg -mrecord-mcount -mfentry -DCC_USING_FENTRY -Wdeclaration-after-statement -Wvla -Wno-pointer-sign -Wno-array-bounds -Wno-stringop-overflow -Wno-restrict -Wno-maybe-uninitialized -fno-strict-overflow -fno-merge-all-constants -fmerge-constants -fno-stack-check -fconserve-stack -Werror=date-time -Werror=incompatible-pointer-types -Werror=designated-init -O1 -Wno-vla -Wno-unused-variable -Wno-unused-value -Wno-unused-label -Wno-unused-parameter -Wno-unused-function -Wno-unused -Wno-date-time -I/home/laurentyin/RTL8821ce/Driver-Realtek-RTL8821CE/include -I/home/laurentyin/RTL8821ce/Driver-Realtek-RTL8821CE/platform -I/home/laurentyin/RTL8821ce/Driver-Realtek-RTL8821CE/hal/btc -DCONFIG_MP_INCLUDED -DCONFIG_POWER_SAVING -DCONFIG_BT_COEXIST -DCONFIG_EFUSE_CONFIG_FILE -DEFUSE_MAP_PATH=\"/system/etc/wifi/wifi_efuse_8821ce.map\" -DWIFIMAC_PATH=\"/data/wifimac.txt\" -DCONFIG_LOAD_PHY_PARA_FROM_FILE -DREALTEK_CONFIG_PATH=\"/lib/firmware/\" -DCONFIG_TXPWR_BY_RATE_EN=2 -DCONFIG_TXPWR_LIMIT_EN=2 -DCONFIG_RTW_ADAPTIVITY_EN=0 -DCONFIG_RTW_ADAPTIVITY_MODE=0 -DHIGH_ACTIVE_HST2DEV=0 -DCONFIG_BR_EXT '-DCONFIG_BR_EXT_BRNAME="'br0'"' -DCONFIG_RTW_NAPI -DCONFIG_RTW_GRO -DCONFIG_RTW_NETIF_SG -DDM_ODM_SUPPORT_TYPE=0x04 -DCONFIG_LITTLE_ENDIAN -DCONFIG_IOCTL_CFG80211 -DRTW_USE_CFG80211_STA_EVENT -I/home/laurentyin/RTL8821ce/Driver-Realtek-RTL8821CE/hal/phydm -DCONFIG_RTL8821C  -DMODULE  -DKBUILD_BASENAME='"rtw_mesh_pathtbl"' -DKBUILD_MODNAME='"8821ce"' -c -o /home/laurentyin/RTL8821ce/Driver-Realtek-RTL8821CE/core/mesh/rtw_mesh_pathtbl.o /home/laurentyin/RTL8821ce/Driver-Realtek-RTL8821CE/core/mesh/rtw_mesh_pathtbl.c
+
+source_/home/laurentyin/RTL8821ce/Driver-Realtek-RTL8821CE/core/mesh/rtw_mesh_pathtbl.o := /home/laurentyin/RTL8821ce/Driver-Realtek-RTL8821CE/core/mesh/rtw_mesh_pathtbl.c
+
+deps_/home/laurentyin/RTL8821ce/Driver-Realtek-RTL8821CE/core/mesh/rtw_mesh_pathtbl.o := \
+    $(wildcard include/config/rtw/mesh.h) \
+    $(wildcard include/config/rtw/mesh/cto/mgate/carrier.h) \
+  include/linux/kconfig.h \
+    $(wildcard include/config/cpu/big/endian.h) \
+    $(wildcard include/config/booger.h) \
+    $(wildcard include/config/foo.h) \
+  include/linux/compiler_types.h \
+    $(wildcard include/config/have/arch/compiler/h.h) \
+    $(wildcard include/config/enable/must/check.h) \
+    $(wildcard include/config/optimize/inlining.h) \
+    $(wildcard include/config/cc/has/asm/inline.h) \
+  include/linux/compiler_attributes.h \
+  include/linux/compiler-gcc.h \
+    $(wildcard include/config/retpoline.h) \
+    $(wildcard include/config/arch/use/builtin/bswap.h) \
+
+/home/laurentyin/RTL8821ce/Driver-Realtek-RTL8821CE/core/mesh/rtw_mesh_pathtbl.o: $(deps_/home/laurentyin/RTL8821ce/Driver-Realtek-RTL8821CE/core/mesh/rtw_mesh_pathtbl.o)
+
+$(deps_/home/laurentyin/RTL8821ce/Driver-Realtek-RTL8821CE/core/mesh/rtw_mesh_pathtbl.o):
